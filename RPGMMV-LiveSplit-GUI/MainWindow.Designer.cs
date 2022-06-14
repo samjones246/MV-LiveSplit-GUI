f3da703c5209a13949bacc0c334c56182da27094
Sam Jones <sam.jones240699@gmail.com>
2022-06-14 04:51:54 +0100

    Add project files.

---
+++ RPGMMV-LiveSplit-GUI/MainWindow.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace RPGMMV_LiveSplit_GUI
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.btnOpenAutosplitter = new System.Windows.Forms.Button();
+            this.btnNewAutosplitter = new System.Windows.Forms.Button();
+            this.lstSplitPoints = new System.Windows.Forms.CheckedListBox();
+            this.btnAddSplitPoint = new System.Windows.Forms.Button();
+            this.btnEditSplitPoint = new System.Windows.Forms.Button();
+            this.btnDeleteSplitPoint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Enabled = false;
+            this.btnInstall.Location = new System.Drawing.Point(12, 12);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(136, 30);
+            this.btnInstall.TabIndex = 0;
+            this.btnInstall.Text = "Install Plugin";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // btnOpenAutosplitter
+            // 
+            this.btnOpenAutosplitter.Location = new System.Drawing.Point(154, 12);
+            this.btnOpenAutosplitter.Name = "btnOpenAutosplitter";
+            this.btnOpenAutosplitter.Size = new System.Drawing.Size(136, 30);
+            this.btnOpenAutosplitter.TabIndex = 1;
+            this.btnOpenAutosplitter.Text = "Open Autosplitter";
+            this.btnOpenAutosplitter.UseVisualStyleBackColor = true;
+            // 
+            // btnNewAutosplitter
+            // 
+            this.btnNewAutosplitter.Location = new System.Drawing.Point(296, 12);
+            this.btnNewAutosplitter.Name = "btnNewAutosplitter";
+            this.btnNewAutosplitter.Size = new System.Drawing.Size(136, 30);
+            this.btnNewAutosplitter.TabIndex = 2;
+            this.btnNewAutosplitter.Text = "New Autosplitter";
+            this.btnNewAutosplitter.UseVisualStyleBackColor = true;
+            this.btnNewAutosplitter.Click += new System.EventHandler(this.btnNewAutosplitter_Click);
+            // 
+            // lstSplitPoints
+            // 
+            this.lstSplitPoints.FormattingEnabled = true;
+            this.lstSplitPoints.Location = new System.Drawing.Point(12, 48);
+            this.lstSplitPoints.Name = "lstSplitPoints";
+            this.lstSplitPoints.Size = new System.Drawing.Size(278, 378);
+            this.lstSplitPoints.TabIndex = 3;
+            // 
+            // btnAddSplitPoint
+            // 
+            this.btnAddSplitPoint.Location = new System.Drawing.Point(296, 48);
+            this.btnAddSplitPoint.Name = "btnAddSplitPoint";
+            this.btnAddSplitPoint.Size = new System.Drawing.Size(136, 30);
+            this.btnAddSplitPoint.TabIndex = 4;
+            this.btnAddSplitPoint.Text = "Add";
+            this.btnAddSplitPoint.UseVisualStyleBackColor = true;
+            this.btnAddSplitPoint.Click += new System.EventHandler(this.btnAddSplitPoint_Click);
+            // 
+            // btnEditSplitPoint
+            // 
+            this.btnEditSplitPoint.Location = new System.Drawing.Point(296, 84);
+            this.btnEditSplitPoint.Name = "btnEditSplitPoint";
+            this.btnEditSplitPoint.Size = new System.Drawing.Size(136, 30);
+            this.btnEditSplitPoint.TabIndex = 5;
+            this.btnEditSplitPoint.Text = "Edit";
+            this.btnEditSplitPoint.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteSplitPoint
+            // 
+            this.btnDeleteSplitPoint.Location = new System.Drawing.Point(296, 120);
+            this.btnDeleteSplitPoint.Name = "btnDeleteSplitPoint";
+            this.btnDeleteSplitPoint.Size = new System.Drawing.Size(136, 30);
+            this.btnDeleteSplitPoint.TabIndex = 6;
+            this.btnDeleteSplitPoint.Text = "Delete";
+            this.btnDeleteSplitPoint.UseVisualStyleBackColor = true;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 438);
+            this.Controls.Add(this.btnDeleteSplitPoint);
+            this.Controls.Add(this.btnEditSplitPoint);
+            this.Controls.Add(this.btnAddSplitPoint);
+            this.Controls.Add(this.lstSplitPoints);
+            this.Controls.Add(this.btnNewAutosplitter);
+            this.Controls.Add(this.btnOpenAutosplitter);
+            this.Controls.Add(this.btnInstall);
+            this.Name = "MainWindow";
+            this.Text = "MV LiveSplit";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInstall;
+        private System.Windows.Forms.Button btnOpenAutosplitter;
+        private System.Windows.Forms.Button btnNewAutosplitter;
+        private System.Windows.Forms.CheckedListBox lstSplitPoints;
+        private System.Windows.Forms.Button btnAddSplitPoint;
+        private System.Windows.Forms.Button btnEditSplitPoint;
+        private System.Windows.Forms.Button btnDeleteSplitPoint;
+    }
+}
+
---
+++ RPGMMV-LiveSplit-GUI/SplitPointEditor.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace RPGMMV_LiveSplit_GUI
+{
+    partial class SplitPointEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblActivators = new System.Windows.Forms.Label();
+            this.btnAddActivator = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 72);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(262, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(49, 17);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(67, 6);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(207, 22);
+            this.txtName.TabIndex = 2;
+            // 
+            // lblActivators
+            // 
+            this.lblActivators.AutoSize = true;
+            this.lblActivators.Location = new System.Drawing.Point(12, 44);
+            this.lblActivators.Name = "lblActivators";
+            this.lblActivators.Size = new System.Drawing.Size(70, 17);
+            this.lblActivators.TabIndex = 3;
+            this.lblActivators.Text = "Activators";
+            // 
+            // btnAddActivator
+            // 
+            this.btnAddActivator.Location = new System.Drawing.Point(88, 39);
+            this.btnAddActivator.Name = "btnAddActivator";
+            this.btnAddActivator.Size = new System.Drawing.Size(75, 27);
+            this.btnAddActivator.TabIndex = 4;
+            this.btnAddActivator.Text = "Add";
+            this.btnAddActivator.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(199, 178);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 27);
+            this.btnOk.TabIndex = 5;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(118, 178);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 27);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // SplitPointEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 213);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnAddActivator);
+            this.Controls.Add(this.lblActivators);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.panel1);
+            this.Name = "SplitPointEditor";
+            this.Text = "Edit Split Point";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblActivators;
+        private System.Windows.Forms.Button btnAddActivator;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
